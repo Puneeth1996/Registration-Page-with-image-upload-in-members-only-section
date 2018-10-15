acdabf2f34c7e5f6a4fffff827ce82e7d622b67b
Puneeth1996 <puneeth1996p@gmail.com>
2018-10-15 10:22:06 +0530

    Commented sql for both DB( registration and testing )
    included sql for registration DB

--- index.php
+++ index.php
@@ -27,6 +27,26 @@ tbl_images
 
 
 
+<!-- Create a database called registration. In the registration database, add a table called users. The users table will take the following four fields.
+
+id
+username  -  varchar(100)
+email  -  varchar(100)
+password  -  varchar(100)
+You can create this using a MySQL client like PHPMyAdmin.
+
+
+Or you can create it on the MySQL prompt using the following SQL script:
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1; -->
+
+
+
 
 <?php 
   
